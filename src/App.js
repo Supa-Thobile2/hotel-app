dd14467de9281d5e0ecee292605cc4860b0c8807
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-31 19:04:15 +0200

    hosted app on firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Rooms from './component/rooms';
 import Contact from './component/contact/Contact'
-import SignIn from './component/login';
-import SignUp from './component/signup';
+// import SignIn from './component/login';
+// import SignUp from './component/signup';
 
 const App = () => {
   return (
     <div>
       {/* <h1>Hopewell's Garden</h1> */}
-      <SignIn/>
-      <SignUp/>
+      {/* <SignIn/> */}
+      {/* <SignUp/> */}
       <Rooms/>
       
       <Contact/>
