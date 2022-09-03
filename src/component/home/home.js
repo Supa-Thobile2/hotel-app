32a8433d3bb4b76b68e7bbbb30672d34529de5f7
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-09-03 10:43:29 +0200

    updated project

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -2,12 +2,14 @@
 // import SignUp from './component/logSignIn/signup.js';
 import Rooms from '../rooms/rooms';
 import Contact from '../contact/Contact'
+import Services from '../services/services'
 
 function Home(props){
     return(
         <div className="Container">
 
             <Rooms/>
+            <Services/>
       
             <Contact/>
 
